410f5e5fd49901d7f87213328d6ce73396872775
kim <ksc0407@khu.ac.kr>
2021-04-04 00:53:08 +0900

    초기 커밋.

---
+++ Library managing prgram/Library managing prgram.cpp
@@ -0,0 +1,19 @@
+﻿// Library managing prgram
+/*The program for Library
+1. Resister books
+2. Renting books
+3. Returning books
+4. List of books
+5. Quit
+*/
+
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
